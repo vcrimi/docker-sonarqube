e318a8475bd6670c4ae72d032b2557f04b1cafa0
Vincenzo Crimi <vincenzo.crimi@doubleslash.de>
2016-09-20 15:31:25 +0200

    added rm commands before updating plugins

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ echo "10.168.11.51 devcenter" >> /etc/hosts ;
 if [ ! -f /opt/sonarqube/data/.import_done ];
 then
     echo "loading extensions..."
-	rm -rf /opt/sonarqube/extensions/plugins/sonar-java-plugin-3.8.jar 
+	#rm -rf /opt/sonarqube/extensions/plugins/sonar-ldap-plugin-*.jar 
 	#wget -O /opt/sonarqube/extensions/plugins/sonar-ldap-plugin-1.5.1.jar   https://sonarsource.bintray.com/Distribution/sonar-ldap-plugin/sonar-ldap-plugin-1.5.1.jar
+	rm -rf /opt/sonarqube/extensions/plugins/sonar-findbugs-plugin-*.jar 
 	wget -O /opt/sonarqube/extensions/plugins/sonar-findbugs-plugin-3.3.jar https://sonarsource.bintray.com/Distribution/sonar-findbugs-plugin/sonar-findbugs-plugin-3.3.jar
+	rm -rf /opt/sonarqube/extensions/plugins/sonar-java-plugin-*.jar 
 	wget -O /opt/sonarqube/extensions/plugins/sonar-java-plugin-3.13.1.jar     https://sonarsource.bintray.com/Distribution/sonar-java-plugin/sonar-java-plugin-3.13.1.jar
 	
 	echo "importing // rules"
